045830c397ba66dafb0096f5feb1869c247ea877
Adelice <nancytoniw@gmail.com>
2019-03-22 16:30:20 +0200

    install bootstrap

--- insta/settings.py
+++ insta/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'instapp'
+    'bootstrap3',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
